44cc2aff0e3630c7554d09333719c968cbf8912f
jysleyin <69206877+jysleyin@users.noreply.github.com>
2021-11-18 12:26:41 -0500

    Update App.vue

--- chang/src/App.vue
+++ chang/src/App.vue
@@ -1,9 +1,13 @@
 
 <template>
   <div class="container">
-    <router-link to="/births">Births</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-view />
+    <div class="buttons">
+      <div class="test">
+      <router-link to="/births"><button> Births</button> </router-link> 
+      <router-link to="/about"><button> About </button> </router-link>
+      <router-view />
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -23,5 +27,25 @@ export default {
 <style>
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
 
+body {
+  margin: 0 auto;
+  background-color: #2A324B;
+}
 
+.test {
+  background-color: #C7CCDB;
+  /* margin: 0 auto; */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+
+.container {
+    margin: 0 auto;
+    position: relative;
+    height: 200px;
+}
 </style>
+
