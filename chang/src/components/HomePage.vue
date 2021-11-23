df82ba569c69023a78fd0d8570766485082abd0a
jysleyin <69206877+jysleyin@users.noreply.github.com>
2021-11-23 12:23:42 -0500

    Create HomePage.vue

---
+++ chang/src/components/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div class="container">
+   <h2> {{ title }} </h2>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            title: 'Welcome to Birthday Match!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h2 {
+  font-family: 'Quicksand', sans-serif;
+  font-size: 2rem;
+  text-align: center;
+  color: #8288A1;
+  margin: auto;
+}
+</style>
