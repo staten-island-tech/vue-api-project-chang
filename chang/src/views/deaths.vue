439ddacd4436f9376d8292c5098cee387c0fc63e
jysleyin <69206877+jysleyin@users.noreply.github.com>
2021-11-23 12:28:55 -0500

    Update deaths.vue

--- chang/src/views/deaths.vue
+++ chang/src/views/deaths.vue
@@ -63,7 +63,7 @@ export default {
         console.log(data);
         console.log(this.deaths);
       } catch (error) {
-        alert(error);
+        alert("Invalid input!");
       }
     },
     submitEvent() {
