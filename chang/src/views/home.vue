db935bfed5d366a71fe44c1a95f876887031121d
jysleyin <julissaliangg@gmail.com>
2021-11-23 20:08:59 -0500

    Create home.vue

---
+++ chang/src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="app">
+  <HomePage />
+    <router-view/>
+</div>
+</template>
+
+<script>
+import HomePage from '@/components/HomePage.vue'
+export default {
+    components: {
+    HomePage,
+  },
+}
+
+</script>
+
+<style>
+
+
+</style>
