e1dcdb47db26bcdece5e894fb3f2271112c18e7b
jysleyin <69206877+jysleyin@users.noreply.github.com>
2021-11-24 11:44:29 -0500

    Update HomePage.vue

--- chang/src/components/HomePage.vue
+++ chang/src/components/HomePage.vue
@@ -2,8 +2,17 @@
 
 <div class="container">
    <h2> {{ title }} </h2>
-   <div class="row"></div>
    <hr>
+   <div class="row">
+       
+       <div class="column">
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       </div>
+       <div class="column2">
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       </div>
+   </div>
+   
 </div>
 
 </template>
@@ -19,6 +28,13 @@ export default {
 </script>
 
 <style scoped>
+.column {
+    background-color:white;
+}
+
+.column1 {
+    background-color:white;
+}
 h2 {
   font-family: 'Quicksand', sans-serif;
   font-size: 2rem;
