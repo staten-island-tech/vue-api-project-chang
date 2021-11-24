36a898dcbbda1bf7980f34c1973cb12b09307c20
jysleyin <julissaliangg@gmail.com>
2021-11-23 20:39:41 -0500

    Create InputData.vue

---
+++ chang/src/components/InputData.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="search-bar">
+
+        <input id="search" type="text" placeholder="Enter Month" v-model="month" />
+        <input id="search" type="text" placeholder="Enter Day" v-model="day"/>
+        <button type="submit"><i class="fa fa-search"></i></button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+input {
+  font-size: 2.28rem;
+}
+
+.search-bar button {
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 2rem;
+  border: none;
+  cursor: pointer;
+}
+
+.search-bar button:hover {
+  background: #ccc;
+}
+
+.search-bar {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
