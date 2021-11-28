a14a4d73ff3cab38e4527ee35722d85f2d4ffda1
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-11-28 16:09:53 -0500

    using switch statements now

--- chang/src/components/SearchFormBirth.vue
+++ chang/src/components/SearchFormBirth.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="search-bar">
-    <form @submit.prevent="fetchData">
+    <form
+      @submit.prevent="
+        switchStatement();
+        fetchData();
+      "
+    >
       <input
         id="search"
         type="text"
@@ -42,6 +47,7 @@ export default {
       month: null,
       isClicked: false,
       apiLength: null,
+      value: "",
     };
   },
   methods: {
@@ -84,6 +90,82 @@ export default {
           "you can't do that because there is nothing to go back to, the only way is forward"
         );
         return;
+      }
+    },
+    switchStatement() {
+      switch (this.month) {
+        case "January":
+          this.month = "1";
+          break;
+        case "january":
+          this.month = "1";
+          break;
+        case "February":
+          this.month = "2";
+          break;
+        case "february":
+          this.month = "2";
+          break;
+        case "March":
+          this.month = "3";
+          break;
+        case "march":
+          this.month = "3";
+          break;
+        case "April":
+          this.month = "4";
+          break;
+        case "april":
+          this.month = "4";
+          break;
+        case "May":
+          this.month = "5";
+          break;
+        case "may":
+          this.month = "5";
+          break;
+        case "June":
+          this.month = "6";
+          break;
+        case "june":
+          this.month = "6";
+          break;
+        case "July":
+          this.month = "7";
+          break;
+        case "july":
+          this.month = "7";
+          break;
+        case "August":
+          this.month = "8";
+          break;
+        case "august":
+          this.month = "8";
+          break;
+        case "September":
+          this.month = "9";
+          break;
+        case "september":
+          this.month = "9";
+          break;
+        case "October":
+          this.month = "10";
+          break;
+        case "october":
+          this.month = "10";
+          break;
+        case "November":
+          this.month = "11";
+          break;
+        case "november":
+          this.month = "11";
+          break;
+        case "December":
+          this.month = "12";
+          break;
+        case "december":
+          this.month = "12";
+          break;
       }
     },
   },
