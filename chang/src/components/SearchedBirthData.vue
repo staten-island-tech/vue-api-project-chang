2b49e309fca691679eb99f6317773dd15e41e044
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-11-27 20:54:43 -0500

    updated string grammar

--- chang/src/components/SearchedBirthData.vue
+++ chang/src/components/SearchedBirthData.vue
@@ -2,7 +2,11 @@
   <div>
     <div class="birth-data">
       <h2>
-        {{ info.births[arrayIndex].description + info.births[arrayIndex].year }}
+        {{
+          info.births[arrayIndex].description +
+          " was born in the year " +
+          info.births[arrayIndex].year
+        }}
       </h2>
     </div>
   </div>
